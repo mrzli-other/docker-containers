80f237a31489dddad1240371c8088e84b0ebc42b
Goran Mrzljak <goran.mrzljak@gmail.com>
2019-07-21 16:29:25 +0200

    Updates to docker-private-registry container

--- docker-private-registry/build-image.sh
+++ docker-private-registry/build-image.sh
@@ -4,4 +4,4 @@ if [[ $1 != "" ]]; then
   TAG=$1
 fi
 # echo ${TAG}
-docker image build -f caddy-for-repository.Dockerfile -t mrzli/caddy-for-repository:${TAG} .
+docker image build -f caddy-for-registry.Dockerfile -t mrzli/caddy-for-registry:${TAG} .
